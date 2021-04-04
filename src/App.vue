24d4de7e7992808dbeea0bc12dffed484c38d3ee
Bence Markus <markusbence@outlook.com>
2021-04-04 19:48:55 +0200

    basic vue structure created

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Dashboard class="app" />
 </template>
 
 <script>
-
 export default {
   name: 'App',
-}
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
---
+++ src/components/dashboard/top/TopRight.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="componentProp" />
+</template>
+
+<script>
+export default {
+  name: 'TopRight',
+  props: {
+    componentProp: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style></style>
